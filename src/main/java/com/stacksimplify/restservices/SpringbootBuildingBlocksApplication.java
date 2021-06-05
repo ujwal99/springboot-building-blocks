0f19cf94c3165e8883b09b5b89ceac560f5f7bf7
ujwal99 <m.ujwal99@gmail.com>
2021-06-05 22:18:38 +0530

    first commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingBlocksApplication.class, args);
+	}
+
+}
